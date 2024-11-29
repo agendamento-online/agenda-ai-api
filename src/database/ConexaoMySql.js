bfb7a9cd92ac7643d8f428f8fd6f6f230ce07f3f
bion23ouriquess <gaby8.bion@gmail.com>
2024-11-28 21:37:18 -0300

    mudando senha

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -4,7 +4,7 @@ const dbConfig = {
   host: process.env.MYSQL_HOST || "localhost",
   port: process.env.MYSQL_PORT || "3306",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PWD || "root",
+  password: process.env.MYSQL_PWD || "Bion2303*",
   database: process.env.MYSQL_DB || "aulas_api_senai",
 };
 
